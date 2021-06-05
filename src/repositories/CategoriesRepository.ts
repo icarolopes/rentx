d1b5b8c0a3140022f117f2309ad7d5687136f45a
Icaro Lopes <icarofernandeslopes@gmail.com>
2021-06-05 16:38:46 -0300

    08 - Validando o cadastro de categoria

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -26,4 +26,9 @@ export class CategoriesRepository {
   index(): Category[] {
     return this.categories
   }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name)
+    return category
+  }
 }
