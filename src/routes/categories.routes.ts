7871cffbc913471f1632da2d2f6b9597c06ad17c
Icaro Lopes <icarofernandeslopes@gmail.com>
2021-06-05 13:09:34 -0300

    04 - Inserindo ID com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express'
+import { v4 as uuidV4 } from 'uuid'
 
 const categoriesRoutes = Router()
 
 const categories = []
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
   categories.push({
+    id: uuidV4(),
     name,
     description
   })
+
+  console.log(categories)
 
   return response.status(201).send()
 })
