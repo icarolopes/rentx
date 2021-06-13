91e67fa3af654f7c43ecbe9bc3f64046c72af5e5
icarolopes <icarofernandeslopes@gmail.com>
2021-06-13 19:49:28 -0300

    06 - Conhecendo Singleton Pattern

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express'
-import { CreateCategoryUseCase } from '../modules/cars/useCases/CreateCategory/CreateCategoryUseCase'
 
-// import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { PostgresCategoriesRepository } from '../modules/cars/repositories/PostgresCategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/CreateCategory'
 import { listCategoriesController } from '../modules/cars/useCases/ListCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new PostgresCategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response)
