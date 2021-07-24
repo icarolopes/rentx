f5e897aedbfe9e940d8c9252e5162903988e1ccf
icarolopes <icarofernandeslopes@gmail.com>
2021-07-24 17:13:08 -0300

    02 - Refatorando as especificações

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase'
-
 export class CreateSpecificationController {
-  constructor(
-    private createSpecificationsUseCase: CreateSpecificationUseCase
-  ) {}
-
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body
 
-    this.createSpecificationsUseCase.execute({ name, description })
+    const createSpecificationUseCase = container.resolve(
+      CreateSpecificationUseCase
+    )
+
+    createSpecificationUseCase.execute({ name, description })
 
     return response.status(201).send()
   }
