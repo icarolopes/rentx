aa87772c4640d863886a29cb615976cb8c2146cd
icarolopes <icarofernandeslopes@gmail.com>
2021-08-08 11:41:41 -0300

    03 - Remover arquivo de avatar existente

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from 'tsyringe'
 
 import { IUsersRepository } from '../../../accounts/repositories/IUsersRepository'
 
+import { deleteFile } from '../../../../utils/file'
+
 interface IRequest {
   user_id: string
   avatar_file: string
@@ -16,6 +18,10 @@ export class UpdateUserAvatarUseCase {
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id)
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
+    }
 
     user.avatar = avatar_file
 
