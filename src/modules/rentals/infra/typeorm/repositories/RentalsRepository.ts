3664b9db6045affdf5be11a4150d53ff465d8e37
icarolopes <icarofernandeslopes@gmail.com>
2021-09-02 22:03:35 -0300

    03 - Caso de uso de devolução de carro

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -35,4 +35,9 @@ export class RentalsRepository implements IRentalsRepository {
 
     return rental
   }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id)
+    return rental
+  }
 }
